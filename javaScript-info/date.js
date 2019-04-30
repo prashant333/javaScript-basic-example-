f13d6c5407c8ffd40693807dd9503f0d449704d0
prashant333 <pmani157@gmail.com>
2019-04-30 07:12:00 +0530

    answers updated

--- javaScript-info/date.js
+++ javaScript-info/date.js
@@ -1,11 +1,11 @@
 // European weekDays
 
-// function europeanWeekdays(date) {
-//     let days=['SUnday','Monday','Tuesday','wednesday','Thursday','Friday','Saturday']
-//     return days[date.getDay()]
-// }
-// let date = new Date(2012, 0, 3);  // 3 Jan 2012
-// console.log( europeanWeekdays(date) );
+function europeanWeekdays(date) {
+    let days=['SUnday','Monday','Tuesday','wednesday','Thursday','Friday','Saturday']
+    return days[date.getDay()]
+}
+let date = new Date(2012, 0, 3);  // 3 Jan 2012
+console.log( europeanWeekdays(date) );
 
 // date substraction
 
