14e2323de8a9efee32f115c54d63f9ace9676e7d
prashant333 <pmani157@gmail.com>
2019-04-30 06:47:23 +0530

    substract date example

--- javaScript-info/date.js
+++ javaScript-info/date.js
@@ -1,8 +1,18 @@
 // European weekDays
 
-function europeanWeekdays(date) {
-    let days=['SUnday','Monday','Tuesday','wednesday','Thursday','Friday','Saturday']
-    return days[date.getDay()]
+// function europeanWeekdays(date) {
+//     let days=['SUnday','Monday','Tuesday','wednesday','Thursday','Friday','Saturday']
+//     return days[date.getDay()]
+// }
+// let date = new Date(2012, 0, 3);  // 3 Jan 2012
+// console.log( europeanWeekdays(date) );
+
+// date substraction
+
+function dateSubstract(date, day) {
+    let date1 = new Date(date)
+    date1.setDate(date1.getDate()-day);
+    return date1;
 }
-let date = new Date(2012, 0, 3);  // 3 Jan 2012
-console.log( europeanWeekdays(date) );
+let date= new Date(2018,0,3)
+console.log(dateSubstract(date,1))
